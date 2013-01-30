d27f1231ce1ec24746471e4b5ef59e4dbd8ddb1b
Charles Care <c.p.care@gmail.com>
2013-01-30 09:04:48 +0000

    Setup for Heroku

--- listApi.js
+++ listApi.js
@@ -1,7 +1,7 @@
 var request = require('request');
 var jsdom = require('jsdom');
 var express = require('express');
-var app = express.createServer();
+var app = express();
 
 var port = process.env.PORT || 3000;
 
